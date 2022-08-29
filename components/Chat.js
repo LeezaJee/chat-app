55eb1d1f6bf971ee695c91c15064d2c784b718d0
Mona Lisa <lisa.nadeem@outlook.de>
2022-08-29 21:48:17 +0200

    added another message from user _id: 2

--- components/Chat.js
+++ components/Chat.js
@@ -20,8 +20,8 @@ export default class Chat extends React.Component {
     this.setState({
       messages: [
         {
-          _id: 1,
-          text: "Hello developer",
+          _id: 2,
+          text: "How are your studies going?",
           createdAt: new Date(),
           user: {
             _id: 2,
@@ -30,7 +30,18 @@ export default class Chat extends React.Component {
           },
         },
         {
-          _id: 2,
+          _id: 1,
+          text: "Hello developer!",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+
+        {
+          _id: 3,
           text: `${this.props.route.params.name} has entered the chat`,
           createdAt: new Date(),
           system: true, // system message
