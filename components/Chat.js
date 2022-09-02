a11b1beb35cc1c92128e96295ecd53281598de79
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-02 12:32:28 +0200

    modified ComponentDidMount to add user authentication

--- components/Chat.js
+++ components/Chat.js
@@ -41,37 +41,41 @@ export default class Chat extends React.Component {
     this.referenceChatMessages = firebase.firestore().collection("messages");
   }
 
+        },
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 2,
-          text: "How are your studies going?",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
-        {
-          _id: 1,
-          text: "Hello developer!",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
-        },
+    // required for listing name in default message
+    // used to display title/name at very top of page
+    let { name } = this.props.route.params;
+    this.props.navigation.setOptions({ title: name });
 
-        {
-          _id: 3,
-          text: `${this.props.route.params.name} has entered the chat`,
-          createdAt: new Date(),
-          system: true, // system message
+    // reference to messages collection
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+    // authentication listener
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+
+      // update user state
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        loggedInText: "You are logged in",
+        user: {
+          _id: user._id,
+          name: name,
+          avatar: "https://placeholder.com/140/140/any",
         },
-      ],
+      });
+      // reference to active messages collection
+      this.referenceMessagesUser = firebase
+        .firestore()
+        .collection("messages")
+        .where("uid", "==", this.state.uid);
+
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
     });
   }
 
