bf5e85836a1af595706a5d3e9ea2f50439d38e03
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-13 14:39:08 +0200

    created new CustomActions file with import statements

---
+++ components/CustomActions.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import * as Location from "expo-location";
+
+import firebase from "firebase";
+import firestore from "firebase";
+import { TouchableOpacity, View, StyleSheet, Text } from "react-native";
+
