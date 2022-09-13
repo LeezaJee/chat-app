3a4eca64d9276e56d24c1a49ba17631c7a82ea50
Mona Lisa <lisa.nadeem@outlook.de>
2022-09-13 14:39:39 +0200

    added pickImage function

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -8,3 +8,23 @@ import firebase from "firebase";
 import firestore from "firebase";
 import { TouchableOpacity, View, StyleSheet, Text } from "react-native";
 
+export default class CustomActions extends React.Component {
+  // allows the user to pick an existing image from their device’s media library
+  pickImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    try {
+      if (status === "granted") {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: "Images",
+        }).catch((error) => console.log(error));
+
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImageFetch(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
